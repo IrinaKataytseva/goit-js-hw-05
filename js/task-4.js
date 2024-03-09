73536f6ea76526fe6ba983e47f9f741d4ba85cb4
IrinaKataytseva <151220079+IrinaKataytseva@users.noreply.github.com>
2024-03-09 22:52:19 +0200

    task 1 , 2 done

--- js/task-4.js
+++ js/task-4.js
@@ -1,35 +1,3 @@
-
-
-
-// function isEnoughCapacity(products, containerSize) {
-//   let totalProducts = 0;
-//   for (let prodkey in products) {
-//       totalProducts += products[prodkey];
-//   }
-//   return totalProducts <= containerSize;
-// }
-
-
-// console.log(
-//   isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
-// ); // true
-
-// console.log(
-//   isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
-// ); // false
-
-// console.log(
-//   isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
-// ); // true
-
-// console.log(
-//   isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
-// ); // false
-
-
-
-
-
 // const getUserNames = (users) => users.map(user => user.name);
 // const flattenedCourses = students.flatMap(student => student.courses);
 const getUserNames = (users) => users.flatMap(user => user.name);
